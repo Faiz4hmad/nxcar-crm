3b985dc26c3d6d4393e21208d9f03bf3f1dd5dfe
Faiz4hmad <22123faiz@gmail.com>
2026-09-23 13:36:19 +0530

    Update db.py

--- db.py
+++ db.py
@@ -2,9 +2,9 @@ import pandas as pd
 import psycopg2
 import streamlit as st
 from datetime import datetime
-
 import urllib.parse as urlparse
 
+@st.cache_resource
 def get_db_connection():
     # Connect directly to Supabase using the Streamlit secret URL string
     return psycopg2.connect(st.secrets["DB_URL"])
